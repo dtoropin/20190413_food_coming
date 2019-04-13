4619168b6561b67370ec1226f6c8d0274bdad351
Denis Toropin <denis.toropin@yandex.ru>
2019-04-14 02:45:44 +0300

    refactoring 2

--- app/js/script.js
+++ app/js/script.js
@@ -2,26 +2,20 @@
 
 	var init = function () {
 		_inverseWorld('.content__header');
-		_countdown('2019.07.01');
-		_setUpListners();
-	};
-
-	var _setUpListners = function () {
-		// прослушка событий
+		_countdown('2019.04.15');
 	};
 
 	var _inverseWorld = function (selector) {
-		$(selector).each(function () {
-			this.innerHTML = this.innerHTML.replace(/(\S+)\s*$/, '<span>$1</span>');
-		});
+		var el = document.querySelector(selector);
+		el.innerHTML = el.innerHTML.replace(/(\S+)\s*$/, '<span>$1</span>');
 	};
 
 	var _countdown = function (dateEnd) {
 		var days, hours, minutes, seconds;
-		var _days = $('#days'),
-			_hours = $('#hours'),
-			_min = $('#min'),
-			_sec = $('#sec');
+		var _days = document.querySelector('#days'),
+			_hours = document.querySelector('#hours'),
+			_min = document.querySelector('#min'),
+			_sec = document.querySelector('#sec');
 
 		target_date = new Date(dateEnd).getTime();
 
@@ -31,6 +25,7 @@
 
 		function calculate() {
 			var current_date = new Date().getTime();
+			if (current_date > target_date) return false;
 			var seconds_left = (target_date - current_date) / 1000;
 
 			days = pad(parseInt(seconds_left / 86400));
@@ -42,10 +37,10 @@
 			minutes = pad(parseInt(seconds_left / 60));
 			seconds = pad(parseInt(seconds_left % 60));
 
-			_days.html(parseInt(days, 10));
-			_hours.html(hours);
-			_min.html(minutes);
-			_sec.html(seconds);
+			_days.innerHTML = parseInt(days, 10);
+			_hours.innerHTML = hours;
+			_min.innerHTML = minutes;
+			_sec.innerHTML = seconds;
 		}
 		function pad(n) {
 			return (n < 10 ? '0' : '') + n;
